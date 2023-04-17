294c260d3567432648a5fccd4890e21c6a258991
Mouad Boufas <98649110+MouadXD@users.noreply.github.com>
2023-04-17 14:53:36 +0000

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_VZX0Y3gV1SEdy6nqmEIPrltx0I7FJwU",
-  authDomain: "blog-project-f2620.firebaseapp.com",
-  projectId: "blog-project-f2620",
-  storageBucket: "blog-project-f2620.appspot.com",
-  messagingSenderId: "538121740213",
-  appId: "1:538121740213:web:e664f556148c66e6dbc3fc",
-  measurementId: "G-D7HQQDGPH4"
+  // Put your config here
 };
 
 const app = initializeApp(firebaseConfig);
